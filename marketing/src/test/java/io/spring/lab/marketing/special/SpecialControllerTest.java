1a23e78950327e1ca6bf56a756d11d814190a7ae
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-22 14:09:45 +0200

    Implemented initial specials controller

--- marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
+++ marketing/src/test/java/io/spring/lab/marketing/special/SpecialControllerTest.java
@@ -3,14 +3,12 @@ package io.spring.lab.marketing.special;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -42,6 +40,6 @@ public class SpecialControllerTest {
         // and
         List<SpecialRepresentation> specials = response.getBody();
         assertThat(specials).hasSize(4);
-        assertThat(specials).contains(new SpecialRepresentation(1L, 3, BigDecimal.valueOf(70.0)));
+        assertThat(specials).contains(new SpecialRepresentation(1L, 3, BigDecimal.valueOf(70)));
     }
 }
