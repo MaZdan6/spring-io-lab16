275d4ea1456a3ce8506b5fc5b072291bc6854549
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-22 09:50:40 +0200

    Added initial demo greeter

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingController {
+
+	@GetMapping("/greet/{name}")
+	Greeting sayHello(@PathVariable String name) {
+		return new Greeting(String.format("Hello %s", name));
+	}
+
+}
+
+class Greeting {
+
+	private String message;
+
+	public Greeting(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
