d612743406f0ee8df0121b3af71373e58ee4d84c
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-22 16:42:46 +0200

    Added exception handling

--- warehouse/src/main/java/io/spring/lab/warehouse/item/ItemController.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/item/ItemController.java
@@ -4,18 +4,25 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.spring.lab.warehouse.error.ErrorMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import static io.spring.lab.warehouse.error.ErrorMessage.messageOf;
+import static io.spring.lab.warehouse.error.ErrorMessage.messageResponseOf;
 import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestController
@@ -48,4 +55,19 @@ public class ItemController {
         return ItemRepresentation.of(items.update(changes.withId(id)));
     }
 
+    @PutMapping("/{id}/stock")
+    public ItemRepresentation updateStock(@PathVariable("id") long id, @RequestBody ItemStockUpdate changes) {
+        return ItemRepresentation.of(items.updateStock(changes.withId(id)));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(NOT_FOUND)
+    public ErrorMessage handle(ItemNotFound e) {
+        return messageOf(e);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handle(OutOfStock e) {
+        return messageResponseOf(BAD_REQUEST, e);
+    }
 }
