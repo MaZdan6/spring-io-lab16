a0e3df7dfe34ce8380746fba0d13f87019ba4e07
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-22 14:54:35 +0200

    Excracted math properties auto-configuration to seperate module

--- autoconfigure/src/main/java/io/spring/lab/math/MathProperties.java
+++ autoconfigure/src/main/java/io/spring/lab/math/MathProperties.java
@@ -2,11 +2,14 @@ package io.spring.lab.math;
 
 import java.math.MathContext;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import lombok.Data;
 
 import static java.math.RoundingMode.HALF_EVEN;
 
 @Data
+@ConfigurationProperties(prefix = "math")
 public class MathProperties {
 
 	/** Precision for Math operations */
