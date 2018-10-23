96598a13274700e53067d8ab42c7052fffb9c648
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-23 16:46:06 +0200

    Extracted store module

---
+++ store/src/test/java/io/spring/lab/store/basket/BasketServiceTest.java
@@ -0,0 +1,18 @@
+package io.spring.lab.store.basket;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasketServiceTest {
+
+    @Test
+    public void shouldUpdateBasketWithRegularPriceItem() {
+
+    }
+
+    @Test
+    public void shouldUpdateBasketWithSpecialPriceItem() {
+
+    }
+}
