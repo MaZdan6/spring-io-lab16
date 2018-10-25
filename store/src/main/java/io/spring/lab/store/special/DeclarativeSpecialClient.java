de4195cce395b069b0a51f4210ea1d24c9ed805f
Darek Kaczyński <kaczynskid@gmail.com>
2018-10-25 13:30:08 +0200

    Added fallback for specials client

--- store/src/main/java/io/spring/lab/store/special/DeclarativeSpecialClient.java
+++ store/src/main/java/io/spring/lab/store/special/DeclarativeSpecialClient.java
@@ -1,5 +1,6 @@
 package io.spring.lab.store.special;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import io.spring.lab.cloud.ConditionalOnFeignClient;
+import io.spring.lab.math.MathProperties;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.TEN;
 
 @Slf4j
 @Component
@@ -29,12 +34,30 @@ public class DeclarativeSpecialClient implements SpecialClient {
         return calculation;
     }
 
-    @FeignClient(name = "marketing", path = "/specials")
+    @FeignClient(name = "marketing", path = "/specials", fallback = FallbackSpecialClient.class)
     interface FeignSpecialClient {
 
         @PostMapping("/{itemId}/calculate")
         SpecialCalculation calculateFor(
                 @PathVariable("itemId") long itemId,
                 @RequestBody SpecialCalculationRequest request);
+    }
+
+    @Component
+    @ConditionalOnFeignClient
+    @AllArgsConstructor
+    static class FallbackSpecialClient implements FeignSpecialClient {
+
+        private MathProperties math;
+
+        @Override // 10% discount as fallback
+        public SpecialCalculation calculateFor(long itemId, SpecialCalculationRequest request) {
+            BigDecimal unitCount = math.bigDecimal(request.getUnitCount());
+            BigDecimal regularPrice = request.getUnitPrice().multiply(unitCount, math.getContext());
+            BigDecimal discountRate = ONE.divide(TEN, math.getContext());
+            BigDecimal discount = regularPrice.multiply(discountRate, math.getContext());
+            BigDecimal fallbackPrice = regularPrice.subtract(discount, math.getContext());
+            return new SpecialCalculation("fallback", fallbackPrice);
+        }
     }
 }
